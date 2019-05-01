135edf3ef18fc41685f4ea9491d6bd2dd342044e
Joseph Chiang <jccofresh@gmail.com>
2019-05-01 03:38:25 +0000

    working day boxes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //I would like an exercise routine I can carry out at the gym or at home. (Which exercises do I execute on which day)
 let request = new XMLHttpRequest(); //exercise image request
-let url = "https://wger.de/api/v2/daysofweek/?format=json"
+let url = "https://wger.de/api/v2/daysofweek/?format=json";
 
 request.open("GET", url, true);
 request.setRequestHeader('Authorization', 'Token 2af6480d6fc89f692fcc471714fe36c76d4d89a2');
@@ -13,41 +13,40 @@ request.onload = function() {
         //condition for when request is succesful, status code of request 
        let days = data.results;
        let values = Object.values(days);
-       
        for(let i=0; i <values.length; i++) {
-            if(rowCounter % 4 == 0){
+            if(rowCounter % 4 == 0 ){
                  row = document.createElement('div');
                  row.className = "row center";
                  daysofweek.appendChild(row);
              }    
+           let flipCard = document.createElement('div'); 
+           flipCard.className = "flip-card";
+           let flipCardInner = document.createElement('div'); 
+           flipCardInner.className = "flip-card-inner";
+           let flipCardFront = document.createElement('div');
+           flipCardFront.className = "flip-card-front"; 
+           let flipCardBack = document.createElement('div');
+           flipCardBack.className = "flip-card-back";
+           //creates the placeholder divs
            let day = document.createElement('p');
-           day.className = "col-2 day inline";
-           day.textContent = values[i].day_of_week;
-           row.appendChild(day);
+           day.className = "day";
+           day.textContent = values[i].day_of_week; 
+           // Front content
+           let dayBack = document.createElement('p');
+           dayBack.className = "day";
+           dayBack.textContent = "back"; 
+           //Back content
+           let col = document.createElement('div');
+           col.className = "col-md-1";
+           flipCardFront.appendChild(day); 
+           flipCardBack.appendChild(dayBack);    
+           flipCardInner.appendChild(flipCardFront);
+           flipCardInner.appendChild(flipCardBack);
+           flipCard.appendChild(flipCardInner);
+           row.appendChild(flipCard);
+           row.appendChild(col);
            rowCounter++;
-           
        }
-        //      let routine = document.createElement('div');
-        //      routine.className = "col-3 day";
-        //      let p = document.createElement('p');
-        //      p.textContent = days.day_of_week;
-        //      routine.appendChild(p);
-        //      day.appendChild(routine);
-        //      rowCounter++;
-    //   forEach(day_of_week => {
-    //         if(rowCounter % 4 == 0){
-    //             let day = document.createElement('div');
-    //             day.className = "row";
-    //             daysofweek.appendChild(day);
-    //         }
-    //         let card = document.createElement('div');
-    //         card.className = "col-3 day";
-    //         let p = document.createElement('p');
-    //         p.textContent = day_of_week;
-    //         card.appendChild(p);
-    //         day.appendChild(card);
-    //         rowCounter++;
-    //   });
     }
 };
 request.send();
@@ -77,7 +76,7 @@ let data = {
 
         },
         {
-            section: "shoulders",
+            section: "Shoulders",
 
         },
         {
@@ -137,11 +136,15 @@ function homeLoad() {
 
 
 
-//window.onload = gymLoad();
 
-// function homeLoad() {
-//      let exercise = document.getElementById("exercise");
-//      let gym = document.getElementById("gym"); 
-//      exercise.textContent = data.homeResults[0].section;
-// }
-// window.onload = homeLoad();
+
+//scroll function
+var shiftWindow = function() {
+     scrollBy(0, -50); 
+ };
+window.addEventListener("hashchange", shiftWindow);
+
+function load() { 
+    if (window.location.hash) 
+    shiftWindow(); 
+}
